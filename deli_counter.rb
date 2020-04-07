7ccbd2ed662df8ba03f74948acf6976ecd4166c9
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-07 22:01:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,16 @@
 # Write your code here.
 
-def line
-
+def line(katz_deli)
+  if katz_deli.count == 0 
+    'The Line is empty'
+  end
 end
 
 def take_a_number
   
 end
+
+def now_serving
+
+end 
+
