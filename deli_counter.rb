e618d3671db0f6ac7faa4d7fced8c47af8737679
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-07 22:04:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else
-    output = "The line is currently: " 
+    output = "The line is currently: "
     katz_deli.each_with_index {|val, index| output += "#{index + 1}. #{val}"}
   end
 end
